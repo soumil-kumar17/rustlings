6430d172245a5cc0df6d132501f45686b5356d54
= <=>
2024-05-09 23:17:59 +0530

    day 1 of rustlings

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,10 +5,11 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    Move(Point),
+    ChangeColor(u8, u8, u8)
 }
 
 struct Point {
@@ -41,9 +42,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
-        // fn function((t, u, p, l, e))
+        match message {
+            Message::Echo(x) => self.echo(x),
+            Message::ChangeColor(a, b, c) => self.change_color((a, b, c)),
+            Message::Move(x) => self.move_position(x),
+            Message::Quit => self.quit()
+        }
     }
 }
 
